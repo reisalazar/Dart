23f3ed6bb8f06c313357212e45212161bd7b45d9
reisalazar <rrsb45@gmail.com>
2022-12-16 17:00:11 -0300

    operators_2

---
+++ fundamentals/operadores_2.dart
@@ -0,0 +1,22 @@
+main() {
+  // Atribuition
+
+  double a = 2;
+  a = a + 3;
+  a += 3;
+  a -= 3;
+  a *= 3;
+  a /= 5;
+  a %= 2;
+
+  print(a);
+
+  // Relational -> Boolean
+
+  print(3 > 2);
+  print(3 >= 3);
+  print(3 < 4);
+  print(3 <= 3);
+  print(3 == 3);
+  print(3 == '3');
+}
