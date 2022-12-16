1d513c3e41d2c92ba71b56ba5261b35326a61000
reisalazar <rrsb45@gmail.com>
2022-12-16 18:51:01 -0300

    generics

---
+++ fundamentals/generics.dart
@@ -0,0 +1,14 @@
+main() {
+  List<String> list = ['banana', 'orange', ' apple'];
+  list.add('melon');
+
+  print(list);
+
+  Map<String, double> salaries = {
+    'manager': 19345.78,
+    'salesman': 16345.78,
+    'intern': 605.78
+  };
+
+  print(salaries);
+}
