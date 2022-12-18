0b20446f0ff3849633682610ce87ace5e439bae1
reisalazar <rrsb45@gmail.com>
2022-12-18 00:07:04 -0300

    switch

---
+++ control/switch.dart
@@ -0,0 +1,20 @@
+import 'dart:math';
+
+main() {
+  var grade = Random().nextInt(11);
+  print("the random grade is $grade");
+
+  switch (grade) {
+    case 10:
+      case9:
+      print("perfect grade");
+      break;
+    case 8:
+      print("Approved!");
+      break;
+    default:
+      print("invalid grade.");
+  }
+
+  print("program end.");
+}
