5fac05b32f157d87d79211860ee6ef3d5174e4f8
reisalazar <rrsb45@gmail.com>
2022-12-20 15:33:24 -0300

    return fun

---
+++ functions/return_fun.dart
@@ -0,0 +1,17 @@
+int Function(int) sumparcial(int a) {
+  int c = 0;
+  return (int b) {
+    return a + b + c;
+  };
+}
+
+void main(List<String> args) {
+  print(sumparcial(2)(10));
+
+  var sumWih10 = sumparcial(10);
+
+  print(sumWih10(3));
+  print(sumWih10(13));
+  print(sumWih10(7));
+  print(sumWih10(19));
+}
